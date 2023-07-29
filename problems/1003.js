ce73272d6feb4c06bc60c4e60103eeef5c1ce486
Felipe silva <125931907+felipegsilva11@users.noreply.github.com>
2023-07-28 23:49:35 -0300

    1002, 1003, 1004, 1005 e 1006 problem

---
+++ problems/1003.js
@@ -0,0 +1,7 @@
+export function problem (lines){
+    const soma = (a, b) => a + b;
+    const resultado = lines.map(Number).reduce(soma);
+
+    console.log (`SOMA = ${resultado}`);
+
+}
---
+++ problems/1004.js
@@ -0,0 +1,7 @@
+export function problem (lines){
+   
+    const [a, b] = lines;
+    const prod = a * b;
+
+    console.log(`PROD = ${prod}`);
+}
